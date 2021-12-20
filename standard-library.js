38aa6222a1f70981489949d032cb1c0ef9b75b3c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-12-20 23:40:22 +0700

    Modules

---
+++ standard-library.js
@@ -0,0 +1,6 @@
+import os from "os";
+
+console.info(os.platform());
+console.table(os.cpus());
+
+// error karena bukan js modules file
---
+++ standard-library.mjs
@@ -0,0 +1,4 @@
+import os from "os";
+
+console.info(os.platform());
+console.table(os.cpus());
