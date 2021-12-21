d66630ce861d0a127d494eabd1d66ea69a6ad613
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-12-21 23:20:26 +0700

    Report

---
+++ report.mjs
@@ -0,0 +1,12 @@
+import process from "process";
+
+process.report.reportOnFatalError = true;
+process.report.reportOnSignal = true;
+process.report.reportOnUncaughtException = true;
+process.report.filename = "report.json";
+
+function sampleError(){
+    throw new Error("Ups");
+}
+
+sampleError();
